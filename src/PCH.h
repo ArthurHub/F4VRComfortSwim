c0fdc82e6fa71d85bf666c446f989e3cf58d6b7b
ArthurHub <5463635+ArthurHub@users.noreply.github.com>
2025-06-18 08:35:51 -0700

    Base skeleton for F4VR CommonLibF4 mod - cmake based - add_subdirectory building of CommonLibF4

---
+++ src/PCH.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define NOMMNOSOUND
+
+#include "F4SE/F4SE.h"
+#include "RE/Fallout.h"
+#include <REL/Relocation.h>
+
+namespace logger = F4SE::log;
+
+using namespace std::literals;
+
+#define DLLEXPORT __declspec(dllexport)
+
+#include "Version.h"
